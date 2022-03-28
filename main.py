d1ebef9da5c185c83926bc52e90b69e428f91361
Elad Sapir <elad.1999.1999@gmail.com>
2022-03-28 22:42:32 +0300

    Class Shekel

---
+++ main.py
@@ -0,0 +1,37 @@
+class Shekel(Coin):
+    """
+    shekel class - coin of New Israeli Shekel
+    """
+
+    def __str__(self):
+        """
+        converts to Shekel to string
+        :return: format of Shekel
+        """
+        return f'{self.getQuantity()}ג‚×'
+
+    def __repr__(self):
+        """
+        function returns a printable representation of Shekel
+        :return: format of printable representation of Shekel
+        """
+        return f'Shekel({self.getQuantity()})'
+
+    def __add__(self, other):
+        """
+        add between Shekel and other Shekel
+        :param other: other Shekel
+        :return: sum of two Shekels
+        """
+        if type(other) == Shekel:
+            return self.getQuantity() + other.getQuantity()
+
+    def __sub__(self, other):
+        """
+        sub between Shekel and other Shekel
+        :param other: other Shekel
+        :return: subtraction result of two Shekels
+        """
+        if type(other) == Shekel:
+            return self.getQuantity() - other.getQuantity()
+
